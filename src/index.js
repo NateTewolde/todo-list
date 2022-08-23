8fb3c1a4ce1d39c5958b6c0b20c2ffc0ae3c70b3
NateTewolde <tewolde.nate@gmail.com>
2022-08-22 21:52:32 -0700

    Add plan for completing todo-list app

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
 console.log("hi there");
+
+//the plan...
+//make a default template similar to the tabs in
+//the resturant project. It will include the top bar and the
+//side bar. Maybe it will be coded into html as it mainly doesnt change,
+//things just get added to it like projects but that part can be js.
+
+//Step 1: Make top and side nav with HTMl, ask in TOP disc if good idea
+//Step 2: Make a module for each new page or screen:
+//  - Inbox, Today, This Week, Important, Projects
+//maybe use library book form reworked for making todolists
