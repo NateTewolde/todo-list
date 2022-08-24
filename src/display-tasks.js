377127a3838b70949244ff4d2d9f4006c21efe2d
NateTewolde <tewolde.nate@gmail.com>
2022-08-24 01:08:45 -0700

    Create refreshTasks() displayAddTasksBtn() and formatTaskBtn() for managing and displaying tasks

--- src/display-tasks.js
+++ src/display-tasks.js
@@ -1,20 +1,17 @@
 import { getTasks } from "./information-holder";
+import addTaskForm from "./add-task-form";
 
 function displayTasks() {
-  console.log("1");
   const content = document.querySelector("#content");
 
   let tasks = getTasks();
-  console.log(tasks[0].getTitle);
   const row = [];
   let tasksContainer = document.createElement("div");
   tasksContainer.setAttribute("id", "tasks-container");
-  console.log("2");
   for (let i = 0; i < tasks.length; i++) {
     row[i] = document.createElement("div");
     row[i].classList.add("task-row");
     let title = tasks[i].getTitle();
-    console.log("title: " + title);
     let description = tasks[i].getDescription();
     let dueDate = tasks[i].getDueDate();
     let priority = tasks[i].getPriority();
@@ -25,4 +22,30 @@ function displayTasks() {
   content.appendChild(tasksContainer);
 }
 
-export { displayTasks };
+function refreshTasks() {
+  removeAllChildNodes(content);
+  displayAddTasksBtn();
+  formatAddTaskBtn();
+  displayTasks();
+}
+
+function displayAddTasksBtn() {
+  const addTasksBtn = document.createElement("button");
+  addTasksBtn.classList.add("add-task-btn");
+  addTasksBtn.textContent = "Add Task";
+  content.appendChild(addTasksBtn);
+}
+
+function formatAddTaskBtn() {
+  const addTaskBtn = document.querySelector(".add-task-btn");
+  addTaskBtn.addEventListener("click", addTaskForm);
+}
+
+//helper function
+function removeAllChildNodes(parent) {
+  while (parent.firstChild) {
+    parent.removeChild(parent.firstChild);
+  }
+}
+
+export { displayTasks, refreshTasks };
