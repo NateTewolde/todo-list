d913b48284b3ef1d269b45dbb96ea1ead1d7e6a5
NateTewolde <tewolde.nate@gmail.com>
2022-08-23 21:56:08 -0700

    Format test-btn for testing display-tasks

--- src/index.js
+++ src/index.js
@@ -2,24 +2,20 @@
 //with their project name next to it. Home will also be like a project
 //that can have tasks added to it.
 
-//  1: Make an add task form module w/button first
-//  2: Make module for creating todo Objects
-//      -How to approach this...
-//         need to get form input from form module to object module.
-//         cant send it directly because OOP principles i think
-//          return it to index.js and index.js sends it.
-import { addTaskForm, submitFormButton } from "./add-task-form";
+import addTaskForm from "./add-task-form";
+import { displayTasks } from "./display-tasks";
+import { getTasks } from "./information-holder";
 
 import "./style.css";
 
-//when a new form is made the submit button is formatted
-//when the submit button is clicked then getTaskInputs collects inputs
-//send to a information holder module
 addTaskBtn();
 function addTaskBtn() {
   const addTaskBtn = document.querySelector(".add-task-btn");
-  addTaskBtn.addEventListener("click", () => {
-    addTaskForm();
-    submitFormButton();
-  });
+  addTaskBtn.addEventListener("click", addTaskForm);
+}
+
+testBtn();
+function testBtn() {
+  const testBtn = document.querySelector(".test-btn");
+  testBtn.addEventListener("click", displayTasks);
 }
