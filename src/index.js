59062394d488cec5701c362d747c0e04c1c5951a
NateTewolde <tewolde.nate@gmail.com>
2022-08-24 20:50:09 -0700

    Manually add example Task objects

--- src/index.js
+++ src/index.js
@@ -4,10 +4,24 @@
 
 import addTaskForm from "./add-task-form";
 import { refreshTasks } from "./display-tasks";
-import { getTasks } from "./information-holder";
+import { getTasks, setTask } from "./information-holder";
 import "./style.css";
 
-refreshTasks();
-//display tasks in home in seperated by priority
+//refreshTasks();
+
+//DONE display tasks in home in seperated by priority
 //then display tasks in home sorted by date
 //then format Today and This week tab's
+
+//example tasks for testing
+setTask("Task2", "Description2", "2022-08-27", "2");
+setTask("Task1", "Description1", "2022-08-21", "1");
+
+setTask("Task4Date2", "Description4", "2022-11-25", "");
+setTask("Task4Date1", "Description4", "2020-08-24", "");
+setTask("Task4Date4", "Description4", "2025-01-01", "");
+setTask("Task4Date3", "Description4", "2023-05-01", "");
+
+setTask("Task3", "Description3", "2022-08-24", "3");
+
+refreshTasks();
