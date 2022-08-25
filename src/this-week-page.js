0ad6ff178b684d81c9faf78dd18473c968abff5b
NateTewolde <tewolde.nate@gmail.com>
2022-08-25 05:08:35 -0700

    Correctly displays the tasks for the current week

---
+++ src/this-week-page.js
@@ -0,0 +1,36 @@
+import { getSortedByDate } from "./information-holder";
+import { isThisWeek, getWeek, lastDayOfWeek } from "date-fns";
+import { displayTasks } from "./display-tasks";
+
+function createThisWeekPage() {
+  const content = document.querySelector("#content");
+
+  const thisWeeksDateObj = new Date().toLocaleDateString("en-us", {
+    weekday: "short",
+    year: "numeric",
+    month: "short",
+    day: "numeric",
+  });
+
+  const thisWeeksDate = document.createElement("div");
+  thisWeeksDate.classList.add("thisWeeks-date");
+  thisWeeksDate.textContent = thisWeeksDateObj;
+  content.appendChild(thisWeeksDate);
+
+  const thisWeeksTasks = getThisWeeksTasks();
+  displayTasks(thisWeeksTasks);
+}
+
+function getThisWeeksTasks() {
+  const tasks = getSortedByDate();
+  const thisWeeksTasksArray = [];
+
+  for (let i = 0; i < tasks.length; i++) {
+    if (isThisWeek(new Date(tasks[i].getDueDate()))) {
+      thisWeeksTasksArray.push(tasks[i]);
+    }
+  }
+  return thisWeeksTasksArray;
+}
+
+export default createThisWeekPage;
