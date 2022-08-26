d4c16ed5c6eac2a2c87641a6ab8f4191f653838e
NateTewolde <tewolde.nate@gmail.com>
2022-08-26 06:06:03 -0700

    Update next steps in comments

--- src/index.js
+++ src/index.js
@@ -57,15 +57,7 @@ function refreshPage() {
   removeAllChildNodes(content);
 }
 
-//DONE display tasks in home in seperated by priority
-//      - maybe change it so that its similar to getSortedByDate()
-//      - to use OOP principles better
-//DONE then display tasks in home sorted by date
-//DONE update addTask button to disappear if an add task form is already on screen
-//DONE then format Today and This week tab's, using resturatn page as example.
-//DONE just format it to be clicked on.
-//DONE then Format Today Page to display all tasks due the current day
-//DONE then format This Week Page to display all tasks due the week
-
-//then add an Edit and Delete button to tasks, maybe easiest to just bring up the form again
+//then format edit btn, maybe easiest to just bring up the form again
+//then make it so that each priority section has a header and remove the priority # from the task itself
+//then add a completed checkmark box
 //then format Projects section, should basically be the ability to make homepages with a different sidebar and header name
