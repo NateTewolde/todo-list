21974f3800e2e01596b6b12e7ce9439ff8c20d98
NateTewolde <tewolde.nate@gmail.com>
2022-09-03 21:02:30 -0700

    Add addGithub() and call in formatTabs after createTodayPage() or after createThisWeekPage()

--- src/index.js
+++ src/index.js
@@ -40,10 +40,33 @@ function formatTabs() {
       }
       if (tabIndex === "1") {
         createTodayPage();
+        addGithub();
       }
       if (tabIndex === "2") {
         createThisWeekPage();
+        addGithub();
       }
     });
   });
+}
+
+function addGithub() {
+  const githubSection = document.createElement("div");
+  githubSection.classList.add("github-section");
+
+  const githubName = document.createElement("div");
+  githubName.classList.add("github-name");
+  githubName.textContent = "Copyright © 2022 NateTewolde";
+  githubSection.appendChild(githubName);
+
+  const githubLink = document.createElement("a");
+  githubLink.setAttribute("href", "https://github.com/NateTewolde");
+
+  const githubIcon = document.createElement("img");
+  githubIcon.src = "../src/styles/images/github-svgrepo-com.svg";
+
+  githubLink.appendChild(githubIcon);
+  githubSection.appendChild(githubLink);
+
+  content.appendChild(githubSection);
 }
