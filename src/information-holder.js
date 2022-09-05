456d1cccdcafaa0271305e0450eeab022d3df520
NateTewolde <tewolde.nate@gmail.com>
2022-09-04 21:12:49 -0700

    Fix bug where wrong task would sometimes be selected for editing by updating local storage and tasks in findTaskIndex() to keep them aligned. Also added taskTrue parameter to setTask() for marking tasks true for the defaults

--- src/information-holder.js
+++ src/information-holder.js
@@ -78,7 +78,7 @@ function taskAttributes(
 }
 
 //uses Task to make objects and pushes them to tasks.
-function setTask(title, description, dueDate, priority, projectId) {
+function setTask(title, description, dueDate, priority, projectId, taskTrue) {
   let dueDateFormatted = dueDate.replace(/-/g, "/");
   let dueDateObj = new Date(dueDateFormatted).toLocaleDateString("en-us", {
     year: "numeric",
@@ -93,8 +93,11 @@ function setTask(title, description, dueDate, priority, projectId) {
     title.replace(/ /g, "_") +
     getRandomIntInclusive()
   ).replace(/[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]/g, "");
-  const completedTracker = [false];
 
+  const completedTracker = [false];
+  if (taskTrue === "yes") {
+    completedTracker.push(true);
+  }
   tasks.push(
     Task(
       taskAttributes(
